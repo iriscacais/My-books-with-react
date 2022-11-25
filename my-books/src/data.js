e4248ed084296fedc269a54fc3dec150487203f2
iriscacais <mandeparairis@gmail.com>
2022-11-24 21:43:35 -0300

    cria aplicacao com react

---
+++ my-books/src/data.js
@@ -0,0 +1,44 @@
+const books = [
+    {
+        id: 1,
+        name: 'A vida secreta das árvores',
+        author: 'Peter Wohlleben',
+        genre: 'Literatura Estrangeira / Não-ficção',
+        pages: 224,
+        image: 'https://img.skoob.com.br/KAqogw9yZlXajbnlLKJPW9dTwHc=/200x/center/top/smart/filters:format(jpeg)/https://skoob.s3.amazonaws.com/livros/656905/A_VIDA_SECRETA_DAS_ARVORES_1487703286656905SK1487703286B.jpg',
+
+    },
+    {
+        id: 2,
+        name: 'O dom da amizade',
+        author: 'Colin Duriez',
+        genre: 'Biografia / Não-ficção',
+        pages: 312,
+        image: 'https://img.skoob.com.br/hKIoaeZBm9FcpQ6ywWFvNWoUaZc=/200x/center/top/smart/filters:format(jpeg)/https://skoob.s3.amazonaws.com/livros/31924/TOLKIEN_E_C_S_LEWIS__O_DOM_DA_AMIZADE_1245266464B.jpg',
+    },
+    {
+        id: 3,
+        name: 'Piranesi',
+        author: 'Susanna Clarke',
+        genre: 'Fantasia / Ficção / Literatura Estrangeira',
+        pages: 256,
+        image: 'https://img.skoob.com.br/PQZ4U_IOPRPoYTzt8RsaE9A1S-A=/200x/center/top/smart/filters:format(jpeg)/https://skoob.s3.amazonaws.com/livros/1105580/PIRANESI_16325317201105580SK-V11632531722B.jpg', 
+    },
+
+    {
+        id: 4,
+        name: 'No ar rarefeito',
+        author: 'Jon Krakauer',
+        genre: 'Literatura Estrangeira / Não-ficção',
+        pages: 288,
+        image: 'https://img.skoob.com.br/9MCTaRgMDfHZQJVfUhW1VGnC2Zg=/200x/center/top/smart/filters:format(jpeg)/https://skoob.s3.amazonaws.com/livros/3504/NO_AR_RAREFEITO_15916307883504SK1591630789B.jpg',
+    },
+
+    {
+        id: 5,
+        name: 'Vita Nostra',
+        author: 'Marina Dyachenko, Sergey Dyachenko',
+        genre: 520,
+        image: 'https://img.skoob.com.br/TR5FQsilL_oRiRnGXVD7dD0gF_I=/200x/center/top/smart/filters:format(jpeg)/https://skoob.s3.amazonaws.com/livros/11968210/VITA_NOSTRA_162793893411968210SK-V11627938935B.jpg'
+    }
+]
